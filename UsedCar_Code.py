169f7f2b13262cef3e3732f488d5532756646a34
Zhuohan-Data <70523526+Zhuohan-Data@users.noreply.github.com>
2020-12-03 17:34:07 +0800

    plot

--- UsedCar_Code.py
+++ UsedCar_Code.py
@@ -7,6 +7,8 @@ import numpy as np
 import pylab as py
 import scipy.stats as stats
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
 #%%
 
 # %%
@@ -38,12 +40,6 @@ def dfChkBasics(dframe, valCnt = False):
       print(f'\n{cnt}: {colname} value_counts(): ')
       print(dframe[colname].value_counts())
       cnt +=1
-
-# %%
-def drop0 (col):
-  for i in col:
-    df.drop(df[df[j]] == 0 or '-')
-  
 # %%
 # Dataset
 # The data in this project contains 31 variables and more than 200,000 used cars' data. 
@@ -51,19 +47,24 @@ def drop0 (col):
 # 150,000 observations will be train set and 50,000 observation will be test set.
 # %%
 df=pd.read_csv("used_car_train_20200313.csv")
+# Origin dataset is too large, so we only research on sample have size 20000.
+df=df.sample(n=20000,random_state=2020)
 #%%
-# Data Processing
+# Read Data
 #
 # Drop meaningless variables
 df=df.iloc[:,:16]
 df=df.drop(columns='name')
-# 
 # %%
+# Check Data
 dfChkBasics(df, valCnt= True)
+
+
+
 # %%
 # Q1 Car Attribute
 # Research the influence of a vehicle’s 
-# Brand, Model, Body Type, Fuel Type and Gearbox Type
+# Brand, Body Type, Fuel Type and Gearbox Type
 # on the value of the vehicle.
 
 
@@ -72,16 +73,16 @@ dfChkBasics(df, valCnt= True)
 # Research the influence of a vehicle’s 
 # Milage, Engine power, whether the car has been Damaged
 # on the value of the vehicle.
-
 # %%
-# EDA
+# Q2.1 EDA
+
+# Q2.1.1 EDA
 
 # Select col and drop NA
 dfQ2 = df.iloc[:,[0,7,8,9,14]]
 dfQ2.set_index('SaleID')
 dfQ2.replace('-',np.nan,inplace=True)
 dfQ2=dfQ2.dropna()
-
 #%%
 # Change the data type
 dfQ2["power"] = dfQ2["power"].astype('float')
@@ -90,16 +91,93 @@ dfQ2["notRepairedDamage"] = dfQ2["notRepairedDamage"].astype('int')
 #%%
 # Change 'notRepairedDamage' to category that only have 0 or 1.
 dfQ2=dfQ2[(dfQ2['notRepairedDamage'] == 0) | (dfQ2['notRepairedDamage'] == 1)]
-dfQ2["notRepairedDamage"] = dfQ2["notRepairedDamage"].astype('category')
 #%%
-# Drop car that power equal 0
+# Drop abnormal value in power, kilometer and price.
 dfQ2=dfQ2.drop(dfQ2[dfQ2['power']==0].index)
 dfQ2=dfQ2.drop(dfQ2[dfQ2['kilometer']==0].index)
-#%%
-# QQPlot    
-sm.qqplot(dfQ2['power'], line ='45') 
-py.show() 
+dfQ2=dfQ2.drop(dfQ2[dfQ2['price']==float].index)
 
+# %%
+# Drop outliers
+dfQ2['power'] = dfQ2['power'][dfQ2['power'].between(dfQ2['power'].quantile(.025), dfQ2['power'].quantile(.975))]
+dfQ2['kilometer'] = dfQ2['kilometer'][dfQ2['kilometer'].between(dfQ2['kilometer'].quantile(.025), dfQ2['kilometer'].quantile(.975))]
+dfQ2['price'] = dfQ2['price'][dfQ2['price'].between(dfQ2['price'].quantile(.025), dfQ2['price'].quantile(.975))]
+dfQ2=dfQ2.dropna()
+
+#%%
+# Kilometer issue
+#
+# We think this variable's stratification might come from a mutiple choice question.
+# And the last choice might be the kilometer that larger than 15km, so we have so many "15km" at here.
+# In order to continue our analysis, we choose to drop most of cars' "kilometer" equal to 15.
+dfQ2=dfQ2.drop(((dfQ2['kilometer']==15).sample(frac=0.7,random_state=1)).index)
+#%%
+# Q2.1.2 Normality Plot
+# QQPlot    
+stats.probplot(dfQ2['power'], dist="norm", plot=py)
+py.show()
+stats.probplot(dfQ2['kilometer'], dist="norm", plot=py)
+py.show()
+
+#%%
+# Histogram
+plt.hist(dfQ2['power'], bins='auto')
+plt.xlabel("Power")
+plt.ylabel("Frequency")
+plt.title("Power Histogram")
+plt.show()
+
+plt.hist(dfQ2['kilometer'], bins='auto')
+plt.xlabel("Kilometer")
+plt.ylabel("Frequency")
+plt.title("Kilometer Histogram")
+plt.show()
+#%%
+# Boxplot
+dfQ2['power'].plot(kind='box')
+plt.show()
+dfQ2['kilometer'].plot(kind='box')
+plt.show()
+
+#%%
+# Q2.2 Visualization
+# Power vs Damage
+# Before, we assume the "power" is the power of after using.
+fuzzykilo = dfQ2['kilometer'] + np.random.normal(0,0.75, size=len(dfQ2['kilometer']))
+fuzzypower = dfQ2['power'] + np.random.normal(0,3.5, size=len(dfQ2['notRepairedDamage']))
+plt.plot(fuzzykilo,fuzzypower, 'o', markersize=8 ,alpha = 0.1)
+plt.xlabel("Kilometer")
+plt.ylabel("Power")
+plt.title("Kilometer vs Power")
+plt.show()
+#%%
+# Price vs Damage
+fuzzykilo = dfQ2['notRepairedDamage'] + np.random.normal(0,0.4, size=len(dfQ2['notRepairedDamage']))
+plt.plot(fuzzykilo,dfQ2['price'], 'o', markersize=8 ,alpha = 0.1)
+plt.xlabel("Damge")
+plt.ylabel("Price")
+plt.title("Price vs Damage")
+plt.show()
+
+#%%
+# Price vs Power
+plt.plot(fuzzypower,dfQ2['price'], 'o', markersize=8 ,alpha = 0.1)
+plt.xlabel("Power")
+plt.ylabel("Price")
+plt.title("Price vs Power")
+plt.show()
+
+#%%
+# 3-dimention
+sns.lmplot('power', 'price', data=dfQ2, hue="notRepairedDamage", x_jitter=3.5, scatter_kws={'alpha': 0.3, 's': 40 } )
+plt.xlabel("Power")
+plt.ylabel("Price")
+plt.title("Price vs Power")
+sns.despine()
+
+#%%
+# Q2.3 Model 
+# 
 #%%
 dfChkBasics(dfQ2, valCnt= True)
 # %%
@@ -110,5 +188,5 @@ dfChkBasics(dfQ2, valCnt= True)
 
 # %%
 # Conclusion
-dfQ2
+
 # %%
